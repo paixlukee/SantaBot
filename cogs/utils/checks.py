ca269586b8ffb79413bc0791d7053aafcd2cee9c
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-11-29 20:15:11 -0500

    Update checks.py

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord.utils
 
 def is_owner_check(message):
-    return message.author.id == "396153668820402197"
+    return message.author.id == '396153668820402197'
 
 def is_owner():
     return commands.check(lambda ctx: is_owner_check(ctx.message))
