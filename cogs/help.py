d0ef2f0191872c333203bec4e0cb764ed823795c
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-11-30 00:00:16 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -24,9 +24,10 @@ class Help(commands.Cog):
         embed = discord.Embed(colour=0x9c0101, description='Hello, I am **Santa**! Here is a list of commands that you are able to use.\n\n'\
         f'`{self.prefix}countdown` - **View the countdown until Christmas**\n'\
         f'`{self.prefix}heartbeats` - **View how many days until Christmas**\n'\
-        f'`{self.prefix}setcc` - **Setup live Christmas countdown messages**\n'\
+        f'`{self.prefix}setcc` - **Setup live Christmas countdown messages** - Admin Only\n'\
+        f'`{self.prefix}remcc` - **Turn off live Christmas countdown messages** - Admin Only\n'\
         f'`{self.prefix}image` - **Show random Christmas-themed image**\n'\
-        f'`{self.prefix}song` - **Show random Christmas song** - Admin Only\n'
+        f'`{self.prefix}song` - **Show random Christmas song**\n'
         )
         #embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
         embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
